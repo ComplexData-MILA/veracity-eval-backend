20e04db79580434600cb83aad92c8b7697554366
William Garneau <willi_garn@hotmail.com>
2024-10-09 13:56:38 -0400

    feat(NORD): Working setup

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,3 +1,4 @@
+# Provider configuration
 terraform {
   required_providers {
     google = {
@@ -20,14 +21,22 @@ provider "google" {
   region  = var.region
 }
 
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+# Get GKE cluster info
+data "google_container_cluster" "my_cluster" {
+  name     = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location
 }
 
+# Configure kubernetes provider with cluster access token
 data "google_client_config" "default" {}
 
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+}
+
+# Network resources
 resource "google_compute_network" "vpc_network" {
   name                    = "misinformation-mitigation-vpc-network"
   auto_create_subnetworks = false
@@ -50,20 +59,7 @@ resource "google_compute_subnetwork" "subnet" {
   }
 }
 
-resource "google_compute_global_address" "private_ip_address" {
-  name          = "misinformation-mitigation-private-ip"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = google_compute_network.vpc_network.id
-}
-
-resource "google_service_networking_connection" "private_vpc_connection" {
-  network                 = google_compute_network.vpc_network.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-}
-
+# GKE Cluster
 resource "google_container_cluster" "primary" {
   name     = "misinformation-mitigation-gke-cluster"
   location = "${var.region}-a"
@@ -95,8 +91,6 @@ resource "google_container_cluster" "primary" {
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
   }
-
-  depends_on = [google_service_networking_connection.private_vpc_connection]
 }
 
 resource "google_container_node_pool" "primary_nodes" {
@@ -111,6 +105,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
     service_account = google_service_account.gke_sa.email
     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+    tags            = ["gke-misinformation-mitigation-gke-cluster"]
 
     workload_metadata_config {
       mode = "GKE_METADATA"
@@ -118,37 +113,21 @@ resource "google_container_node_pool" "primary_nodes" {
   }
 }
 
-resource "google_compute_firewall" "allow_internal" {
-  name    = "allow-internal"
+resource "google_compute_firewall" "allow_http_https" {
+  name    = "allow-http-https"
   network = google_compute_network.vpc_network.name
 
   allow {
     protocol = "tcp"
-  }
-  allow {
-    protocol = "udp"
-  }
-  allow {
-    protocol = "icmp"
+    ports    = ["80", "443"]
   }
 
-  source_ranges = ["10.2.0.0/16", "10.20.0.0/16", "10.30.0.0/16"]
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["gke-misinformation-mitigation-gke-cluster"]
 }
 
-resource "google_compute_router" "router" {
-  name    = "nat-router"
-  network = google_compute_network.vpc_network.name
-  region  = var.region
-}
 
-resource "google_compute_router_nat" "nat" {
-  name                               = "nat-config"
-  router                             = google_compute_router.router.name
-  region                             = var.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
-
+# Service Account for GKE
 resource "google_service_account" "gke_sa" {
   account_id   = "misinfo-mitigation-gke-sa"
   display_name = "GKE Service Account"
@@ -158,15 +137,31 @@ resource "google_project_iam_member" "gke_sa_roles" {
   for_each = toset([
     "roles/container.developer",
     "roles/cloudsql.client",
+    "roles/cloudsql.instanceUser",
     "roles/storage.objectViewer"
   ])
-  
+
   project = var.project_id
   role    = each.key
   member  = "serviceAccount:${google_service_account.gke_sa.email}"
 }
 
-resource "google_sql_database_instance" "misinformation-mitigation_db" {
+# Cloud SQL
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "google_sql_database_instance" "misinformation_mitigation_db" {
   name             = "misinformation-mitigation-db"
   database_version = "POSTGRES_13"
   region           = var.region
@@ -180,72 +175,52 @@ resource "google_sql_database_instance" "misinformation-mitigation_db" {
       ipv4_enabled    = false
       private_network = google_compute_network.vpc_network.id
     }
-
-    backup_configuration {
-      enabled = true
-    }
   }
 
   deletion_protection = false
 }
 
-resource "google_sql_database" "misinformation-mitigation_database" {
+resource "google_sql_database" "misinformation_mitigation_database" {
   name     = "misinformation_mitigation_db"
-  instance = google_sql_database_instance.misinformation-mitigation_db.name
+  instance = google_sql_database_instance.misinformation_mitigation_db.name
 }
 
-resource "google_sql_user" "misinformation-mitigation_user" {
+resource "google_sql_user" "misinformation_mitigation_user" {
   name     = "misinformation_mitigation_user"
-  instance = google_sql_database_instance.misinformation-mitigation_db.name
+  instance = google_sql_database_instance.misinformation_mitigation_db.name
   password = var.db_password
 }
 
-resource "google_compute_global_address" "api_ip" {
-  name         = "misinformation-mitigation-api-ip"
-  address_type = "EXTERNAL"
-}
-
-resource "google_dns_managed_zone" "veri_fact_ai" {
-  name        = "veri-fact-ai"
-  dns_name    = "veri-fact.ai."
-  description = "Veri-Fact.ai DNS zone"
-}
-
-resource "google_dns_record_set" "api" {
-  name         = "api.${google_dns_managed_zone.veri_fact_ai.dns_name}"
-  managed_zone = google_dns_managed_zone.veri_fact_ai.name
-  type         = "A"
-  ttl          = 300
-
-  rrdatas = [google_compute_global_address.api_ip.address]
-}
-
-data "kubernetes_service_account" "misinformation-mitigation-api" {
+# Kubernetes Resources
+resource "kubernetes_namespace" "misinformation_mitigation" {
   metadata {
-    name      = "misinformation-mitigation-api"
-    namespace = "default"
+    name = "misinformation-mitigation"
   }
 }
 
-resource "kubernetes_annotations" "service_account_annotation" {
-  api_version = "v1"
-  kind        = "ServiceAccount"
+resource "kubernetes_service_account" "workload_identity_sa" {
   metadata {
-    name      = "misinformation-mitigation-api"
-    namespace = "default"
-  }
-  annotations = {
-    "iam.gke.io/gcp-service-account" = google_service_account.gke_sa.email
-  }
-}
-
-resource "kubernetes_deployment" "misinformation-mitigation-api" {
-  metadata {
-    name      = "misinformation-mitigation-api"
-    namespace = "default"
-    labels = {
-      app = "misinformation-mitigation-api"
+    name      = "workload-identity-sa"
+    namespace = kubernetes_namespace.misinformation_mitigation.metadata[0].name
+    annotations = {
+      "iam.gke.io/gcp-service-account" = google_service_account.gke_sa.email
     }
+  }
+}
+
+resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
+  service_account_id = google_service_account.gke_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "serviceAccount:${var.project_id}.svc.id.goog[${kubernetes_namespace.misinformation_mitigation.metadata[0].name}/${kubernetes_service_account.workload_identity_sa.metadata[0].name}]",
+  ]
+}
+
+resource "kubernetes_deployment" "misinformation_mitigation_api" {
+  metadata {
+    name      = "misinformation-mitigation-api"
+    namespace = kubernetes_namespace.misinformation_mitigation.metadata[0].name
   }
 
   spec {
@@ -265,7 +240,7 @@ resource "kubernetes_deployment" "misinformation-mitigation-api" {
       }
 
       spec {
-        service_account_name = data.kubernetes_service_account.misinformation-mitigation-api.metadata[0].name
+        service_account_name = kubernetes_service_account.workload_identity_sa.metadata[0].name
 
         container {
           image = "gcr.io/${var.project_id}/misinformation-mitigation-api:latest"
@@ -280,22 +255,22 @@ resource "kubernetes_deployment" "misinformation-mitigation-api" {
             container_port = 8001
           }
 
-          lifecycle {
-            post_start {
-              exec {
-                command = ["/bin/sh", "-c", "alembic upgrade head"]
-              }
+          readiness_probe {
+            http_get {
+              path = "/v1/health"
+              port = 8001
             }
+            initial_delay_seconds = 10
+            period_seconds        = 5
           }
         }
 
         container {
           name  = "cloud-sql-proxy"
           image = "gcr.io/cloudsql-docker/gce-proxy:1.33.1"
-          
           command = [
             "/cloud_sql_proxy",
-            "-instances=${var.project_id}:${var.region}:${google_sql_database_instance.misinformation-mitigation_db.name}=tcp:5432",
+            "-instances=${var.project_id}:${var.region}:${google_sql_database_instance.misinformation_mitigation_db.name}=tcp:5432",
             "-ip_address_types=PRIVATE"
           ]
 
@@ -306,25 +281,19 @@ resource "kubernetes_deployment" "misinformation-mitigation-api" {
       }
     }
   }
-
-  lifecycle {
-    ignore_changes = [
-      spec[0].template[0].spec[0].container[0].image,
-      spec[0].template[0].spec[0].container[1].image,
-      metadata[0].annotations
-    ]
-  }
 }
 
-resource "kubernetes_service" "misinformation-mitigation-api-service" {
+resource "kubernetes_service" "misinformation_mitigation_api" {
   metadata {
-    name      = "misinformation-mitigation-api-service"
-    namespace = "default"
+    name      = "misinformation-mitigation-api"
+    namespace = kubernetes_namespace.misinformation_mitigation.metadata[0].name
   }
+
   spec {
     selector = {
-      app = kubernetes_deployment.misinformation-mitigation-api.metadata[0].labels.app
+      app = kubernetes_deployment.misinformation_mitigation_api.spec[0].template[0].metadata[0].labels.app
     }
+
     port {
       port        = 80
       target_port = 8001
@@ -333,28 +302,48 @@ resource "kubernetes_service" "misinformation-mitigation-api-service" {
   }
 }
 
-resource "kubernetes_ingress_v1" "misinformation-mitigation-api-ingress" {
+
+# Load Balancer and DNS
+resource "google_compute_global_address" "misinformation_mitigation_api_ip" {
+  name = "misinformation-mitigation-api-ip"
+}
+
+resource "google_compute_managed_ssl_certificate" "misinformation_mitigation_cert" {
+  name = "misinformation-mitigation-cert"
+  managed {
+    domains = ["api.veri-fact.ai"]
+  }
+}
+
+resource "kubernetes_ingress_v1" "misinformation_mitigation_ingress" {
   metadata {
-    name      = "misinformation-mitigation-api-ingress"
-    namespace = "default"
+    name      = "misinformation-mitigation-ingress"
+    namespace = kubernetes_namespace.misinformation_mitigation.metadata[0].name
     annotations = {
       "kubernetes.io/ingress.class"                 = "gce"
-      "kubernetes.io/ingress.global-static-ip-name" = google_compute_global_address.api_ip.name
-      "networking.gke.io/managed-certificates"      = "misinformation-mitigation-api-cert"
+      "kubernetes.io/ingress.global-static-ip-name" = google_compute_global_address.misinformation_mitigation_api_ip.name
+      "networking.gke.io/managed-certificates"      = google_compute_managed_ssl_certificate.misinformation_mitigation_cert.name
       "kubernetes.io/ingress.allow-http"            = "false"
     }
   }
-
   spec {
+    default_backend {
+      service {
+        name = kubernetes_service.misinformation_mitigation_api.metadata[0].name
+        port {
+          number = 80
+        }
+      }
+    }
     rule {
       host = "api.veri-fact.ai"
       http {
         path {
-          path = "/*"
+          path      = "/"
           path_type = "Prefix"
           backend {
             service {
-              name = kubernetes_service.misinformation-mitigation-api-service.metadata[0].name
+              name = kubernetes_service.misinformation_mitigation_api.metadata[0].name
               port {
                 number = 80
               }
@@ -366,31 +355,92 @@ resource "kubernetes_ingress_v1" "misinformation-mitigation-api-ingress" {
   }
 }
 
+resource "google_dns_managed_zone" "veri_fact_ai" {
+  name        = "veri-fact-ai-zone"
+  dns_name    = "veri-fact.ai."
+  description = "DNS zone for veri-fact.ai"
+}
+
+resource "google_dns_record_set" "api" {
+  name         = "api.${google_dns_managed_zone.veri_fact_ai.dns_name}"
+  managed_zone = google_dns_managed_zone.veri_fact_ai.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = [google_compute_global_address.misinformation_mitigation_api_ip.address]
+}
+
+# Variables
 variable "project_id" {
   description = "The project ID to deploy to"
   default     = "misinformation-mitigation"
 }
 
 variable "region" {
-  description = "The region to deploy resources"
+  description = "The region to deploy to"
   default     = "northamerica-northeast1"
 }
 
 variable "db_password" {
-  description = "The password for the database user"
+  description = "Database password"
   sensitive   = true
 }
 
+# Outputs
 output "kubernetes_cluster_name" {
   value       = google_container_cluster.primary.name
   description = "GKE Cluster Name"
 }
 
 output "database_connection_name" {
-  value = "${var.project_id}:${var.region}:${google_sql_database_instance.misinformation-mitigation_db.name}"
+  value = google_sql_database_instance.misinformation_mitigation_db.connection_name
 }
 
 output "api_ip_address" {
-  value       = google_compute_global_address.api_ip.address
+  value       = google_compute_global_address.misinformation_mitigation_api_ip.address
   description = "The IP address for the API"
+}
+
+resource "google_project_iam_member" "user_project_owner" {
+  project = var.project_id
+  role    = "roles/owner"
+  member  = "user:wgarneau@veri-fact.ai"
+}
+
+resource "kubernetes_cluster_role_binding" "user_cluster_admin" {
+  metadata {
+    name = "user-cluster-admin"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "User"
+    name      = var.user_email
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_role_binding" "user_namespace_admin" {
+  metadata {
+    name      = "user-namespace-admin"
+    namespace = kubernetes_namespace.misinformation_mitigation.metadata[0].name
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "admin"
+  }
+  subject {
+    kind      = "User"
+    name      = var.user_email
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+variable "user_email" {
+  description = "Email of the user to grant permissions"
+  default     = "wgarneau@veri-fact.ai"
 }
