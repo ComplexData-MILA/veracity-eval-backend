2a26f3b1ed6cb86c80e87e42dee6827cfd8fd8b9
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-09-11 10:45:19 -0400

    redo the middleware

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 from app.api.router import router
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.auth.auth0_middleware import Auth0Middleware
-from app.services.user_service import UserService
-from app.repositories.implementations.user_repository import UserRepository
-from app.db.session import AsyncSessionLocal
+# from app.services.user_service import UserService
+# from app.repositories.implementations.user_repository import UserRepository
+# from app.db.session import AsyncSessionLocal
 import logging
 
 formatter = logging.Formatter(fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
@@ -21,17 +21,17 @@ logging.getLogger("uvicorn.error").setLevel(logging.ERROR)
 logging.getLogger("fastapi").setLevel(logging.WARNING)
 
 
-async def get_user_service():
-    async with AsyncSessionLocal() as session:
-        user_repo = UserRepository(session)
-        return UserService(user_repo)
+# async def get_user_service_startup():
+#     async with AsyncSessionLocal() as session:
+#         user_repo = UserRepository(session)
+#         return UserService(user_repo)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logging.info("API Starting up")
-    user_service = await get_user_service()
-    app.state.auth_middleware = Auth0Middleware(user_service)
+    # user_service = await get_user_service_startup()
+    app.state.auth_middleware = Auth0Middleware()
     yield
     logging.info("API Shutting down")
 
